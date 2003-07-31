f214ec06d2d61c5a9f19f6511e53f49be515c56d
Scott Arrington <sarringt@src.gnome.org>
2003-07-31 23:44:55 +0000

    a few windows-specific things
    svn path=/trunk/; revision=141

--- gperl.h
+++ gperl.h
@@ -26,6 +26,14 @@
 #include "perl.h"
 #include "XSUB.h"
 
+#ifdef WIN32
+  /* perl and glib disagree on a few macros... let the wookie win. */
+# undef pipe
+# undef malloc
+# undef realloc
+# undef free
+#endif
+
 #include <glib-object.h>
 
 /*
