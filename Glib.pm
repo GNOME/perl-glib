367b4897cec7b0260c56f9c4eb32e4b693c13245
Scott Arrington <sarringt@src.gnome.org>
2003-09-02 00:06:39 +0000

    don't set dl_load_flags on darwin, it generates a warning. also bump version so that cvs versions of Gtk2 which depend on T_GPERL_GENERIC_WRAPPER will force upgrades.
    svn path=/trunk/; revision=188

--- Glib.pm
+++ Glib.pm
@@ -28,9 +28,9 @@ use warnings;
 require DynaLoader;
 our @ISA = qw(DynaLoader);
 
-our $VERSION = '0.96';
+our $VERSION = '0.97';
 
-sub dl_load_flags { 0x01 }
+sub dl_load_flags { $^O eq 'darwin' ? 0x00 : 0x01 }
 
 bootstrap Glib $VERSION;
 
