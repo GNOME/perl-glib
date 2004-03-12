2832733c9c896bba8667b5978ccdf7bce0a74fbb
Scott Arrington <sarringt@src.gnome.org>
2004-03-12 21:02:43 +0000

    1.04 will be released from the stable-1-04 branch; bump HEAD's version to 1.050.
    svn path=/trunk/; revision=582

--- Glib.pm
+++ Glib.pm
@@ -61,7 +61,7 @@ our %EXPORT_TAGS = (
 our @EXPORT_OK = map { @$_ } values %EXPORT_TAGS;
 $EXPORT_TAGS{all} = \@EXPORT_OK;
 
-our $VERSION = '1.038';
+our $VERSION = '1.050';
 
 sub dl_load_flags { $^O eq 'darwin' ? 0x00 : 0x01 }
 
