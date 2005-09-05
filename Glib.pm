d7780cf67ff465ed3e619a5083ea5b8223f9e547
Torsten Schönfeld <tsch@src.gnome.org>
2005-09-05 18:39:41 +0000

    Remove META.yml.
    	* MANIFEST, META.yml: Remove META.yml.
    
    	* Glib.pm, Makefile.PL, NEWS, README: Stable release 1.100.
    
    svn path=/trunk/; revision=742

--- Glib.pm
+++ Glib.pm
@@ -61,7 +61,7 @@ our %EXPORT_TAGS = (
 our @EXPORT_OK = map { @$_ } values %EXPORT_TAGS;
 $EXPORT_TAGS{all} = \@EXPORT_OK;
 
-our $VERSION = '1.093';
+our $VERSION = '1.100';
 
 sub dl_load_flags { $^O eq 'darwin' ? 0x00 : 0x01 }
 
