ce250ce47cd31dfca2d05cfbc2299f4eeacfd28c
Torsten Schönfeld <tsch@src.gnome.org>
2006-07-23 10:22:41 +0000

    Unstable release 1.131.
    	* Glib.pm, Makefile.PL, NEWS, README: Unstable release 1.131.
    
    svn path=/trunk/; revision=832

--- Glib.pm
+++ Glib.pm
@@ -63,7 +63,7 @@ our %EXPORT_TAGS = (
 our @EXPORT_OK = map { @$_ } values %EXPORT_TAGS;
 $EXPORT_TAGS{all} = \@EXPORT_OK;
 
-our $VERSION = '1.130';
+our $VERSION = '1.131';
 
 sub dl_load_flags { $^O eq 'darwin' ? 0x00 : 0x01 }
 
