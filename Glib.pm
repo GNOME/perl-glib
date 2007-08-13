fe4fbe736636f8e8e7221e939fffab9fbf3f7201
Torsten Schönfeld <tsch@src.gnome.org>
2007-08-13 19:00:00 +0000

    Glib.pm NEWS Unstable release 1.153.
    	* Glib.pm
    	* NEWS
    	* README: Unstable release 1.153.
    
    svn path=/trunk/; revision=886

--- Glib.pm
+++ Glib.pm
@@ -63,7 +63,7 @@ our %EXPORT_TAGS = (
 our @EXPORT_OK = map { @$_ } values %EXPORT_TAGS;
 $EXPORT_TAGS{all} = \@EXPORT_OK;
 
-our $VERSION = '1.152';
+our $VERSION = '1.153';
 
 sub dl_load_flags { $^O eq 'darwin' ? 0x00 : 0x01 }
 
