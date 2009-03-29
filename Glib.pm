d762ad946c47c6e16adf155b0aa07cb30900c094
Torsten Schönfeld <tsch@src.gnome.org>
2009-03-29 14:35:14 +0000

    Stable release 1.221.
    svn path=/trunk/; revision=1117

--- Glib.pm
+++ Glib.pm
@@ -66,7 +66,7 @@ our %EXPORT_TAGS = (
 our @EXPORT_OK = map { @$_ } values %EXPORT_TAGS;
 $EXPORT_TAGS{all} = \@EXPORT_OK;
 
-our $VERSION = '1.220';
+our $VERSION = '1.221';
 
 sub dl_load_flags { $^O eq 'darwin' ? 0x00 : 0x01 }
 
