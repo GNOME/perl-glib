3ecae5c8706b0c61087ae504562e4b304e98fb01
Torsten Schönfeld <kaffeetisch@gmx.de>
2011-08-23 20:59:46 +0200

    Release 1.232

--- lib/Glib.pm
+++ lib/Glib.pm
@@ -66,7 +66,7 @@ our %EXPORT_TAGS = (
 our @EXPORT_OK = map { @$_ } values %EXPORT_TAGS;
 $EXPORT_TAGS{all} = \@EXPORT_OK;
 
-our $VERSION = '1.231';
+our $VERSION = '1.232';
 
 sub dl_load_flags { $^O eq 'darwin' ? 0x00 : 0x01 }
 
