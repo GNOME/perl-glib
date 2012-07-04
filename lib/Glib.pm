e5b63b8e63d49436f93cc9fc7253749d5e770db1
Brian Manning <bmanning@src.gnome.org>
2012-07-04 14:30:48 -0700

    Stable release 1.261

--- lib/Glib.pm
+++ lib/Glib.pm
@@ -67,7 +67,7 @@ our %EXPORT_TAGS = (
 our @EXPORT_OK = map { @$_ } values %EXPORT_TAGS;
 $EXPORT_TAGS{all} = \@EXPORT_OK;
 
-our $VERSION = '1.260';
+our $VERSION = '1.261';
 
 sub dl_load_flags { $^O eq 'darwin' ? 0x00 : 0x01 }
 
